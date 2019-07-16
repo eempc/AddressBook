89a81cb741b0b9ed76de5593516400339ce8f674
eempc <evc484@gmail.com>
2019-07-16 11:37:57 +0100

    prototype working

---
+++ AddressBook2/Data/Migrations/20190716090943_Annotations.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AddressBook2.Data.Migrations
+{
+    public partial class Annotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Contact",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ForeignKey",
+                table: "Contact",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Contact",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Contact",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ForeignKey",
+                table: "Contact",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Contact",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ AddressBook2/Pages/Addresses/Create.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AddressBook2.Data;
+using AddressBook2.Models;
+using Microsoft.AspNetCore.Authorization;
+
+using System.Security.Claims;
+
+namespace AddressBook2.Pages.Addresses {
+    [Authorize]
+    public class CreateModel : PageModel {
+        private readonly AddressBook2.Data.ApplicationDbContext _context;
+
+        // Attempts to find out who is the logged in user and their details
+        public string userId;
+        public string userEmail;
+
+        public CreateModel(AddressBook2.Data.ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public IActionResult OnGet() {
+            userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            userEmail = User.FindFirst(ClaimTypes.Name)?.Value;
+            return Page();
+        }
+
+        [BindProperty]
+        public Contact Contact { get; set; }
+
+        public async Task<IActionResult> OnPostAsync() {
+            if (!ModelState.IsValid) {
+                userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                userEmail = User.FindFirst(ClaimTypes.Name).Value;
+                return Page();
+            }
+
+            _context.Contact.Add(Contact);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
